7118f39b1468bdeb603303649d0a9c354b89f738
RobiR22 <98790945+RobiR22@users.noreply.github.com>
2022-03-31 11:36:49 -0400

    Update hash.py

--- hash.py
+++ hash.py
@@ -7,6 +7,31 @@
 #imports
 import os
 
+def counter(file):
+    count = 0
+    for char in file:
+        count = count + 1
+    return(count)
+  
+def fileFilter():
+    dot = "."
+    count = counter(file)
+    if count >= 3:
+        if file[-2] == dot:
+          #hashFile
+    if count >= 4:
+        if file[-3] == dot:
+            #hashFile
+    if count >= 5:
+        if file[-4]== dot:
+            #hashFile
+    if count >= 6:
+        if file[-5] == dot:
+            #hashFile....................................................................................
+    
+    return(fileFilter())  
+  
+
 #function to go thru directory for files and other directories. Print directory send files to foundFile()
 function thruDirectory(strDirectory):
   print(strDirectory)
